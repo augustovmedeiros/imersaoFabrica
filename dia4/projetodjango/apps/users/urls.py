d42ccedb4550864f70622325aaffbcc6a802218e
Augusto <augustomedeiros71@gmail.com>
2023-08-31 23:05:16 -0300

    Dia 4 - Adicionado users na api

---
+++ dia4/projetodjango/apps/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+from apps.users.api.viewsets import UserViewSet
+
+router = routers.DefaultRouter()
+
+router.register("", UserViewSet, basename="user")
+
+urlpatterns = [
+    path("", include(router.urls))
+]
